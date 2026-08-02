50d04bfbba16ae93964953051c5147270b7e3191
Haris Michelogiannakis <tmichelogiannakis@gmail.com>
2026-08-02 15:31:20 +0300

    refactor: folder structure

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,7 +2,7 @@ import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/
 import { provideHttpClient } from '@angular/common/http';
 import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
-import { providePicsumImageLoader } from './shared/picsum-image-loader';
+import { providePicsumImageLoader } from './core/providers/picsum-image-loader';
 
 export const appConfig: ApplicationConfig = {
   providers: [
